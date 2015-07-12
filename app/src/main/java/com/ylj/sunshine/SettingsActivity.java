4ee39a1bb5aaddce70a7ae83f1d3409bde3a2afe
yulingjie <lingjiey@gmail.com>
2015-07-12 18:46:52 +0800

    add location in settings

--- app/src/main/java/com/ylj/sunshine/SettingsActivity.java
+++ app/src/main/java/com/ylj/sunshine/SettingsActivity.java
@@ -10,7 +10,7 @@ public class SettingsActivity extends PreferenceActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preference);
+        addPreferencesFromResource(R.xml.pref_general);
     }
 
     @Override
